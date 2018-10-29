96c34177085e02c1386c15c0f67b077717925f63
Fernando <fmti91@hotmail.com>
2018-10-29 20:06:11 -0300

    change icon and create variable

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public username: string = "My Name";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
