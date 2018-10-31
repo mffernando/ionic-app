a05fea47cc60e1c29c243be547cfc32bbd45302a
Fernando <fmti91@hotmail.com>
2018-10-31 17:48:12 -0300

    intro page with slide

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,6 +4,7 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { FeedPage } from '../feed/feed';
+import { IntroPage } from '../intro/intro';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -11,9 +12,10 @@ import { FeedPage } from '../feed/feed';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  //tab2Root = AboutPage;
+  //tab3Root = ContactPage;
   tab4Root = FeedPage;
+  tab5Root = IntroPage;
 
   constructor() {
 
